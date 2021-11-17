dd193e7d05eb42dc64fc309ded4cfd99bae4ad07
luanaugusto.atl@gmail.com <luanaugusto.atl@gmail.com>
2021-11-16 22:15:38 -0300

    Atualização da rota: dashboard do participante

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,4 +11,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('participant/dashboard', [DashboardController::class, 'index'])->name('participant.dashboard.index');
+Route::get('participant/dashboard', [App\Http\Controllers\Participant\Dashboard\DashboardController::class, 'index'])->name('participant.dashboard.index');
