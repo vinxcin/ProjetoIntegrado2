d685265b005c74c492447418e22e131834b6930c
Guilherme Henrique <guilhermehsantos2412@gmail.com>
2021-12-03 19:23:35 -0300

    Criação da Request para verificação dos dados e mudança da linguaguem para 'pt-BR'

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'cpf' => 'required',
+            
+        ];
+    }
+}
